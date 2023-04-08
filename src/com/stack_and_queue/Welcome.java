79c7ef7aca09fd4ec5642d910d135e3a8af0999a
sharad2512 <shnarute95@gmail.com>
2023-04-08 11:26:44 +0530

    Welcome message to stack and queue data structure programing

---
+++ src/com/stack_and_queue/Welcome.java
@@ -0,0 +1,7 @@
+package com.stack_and_queue;
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome message to stack and queue data structure");
+    }
+}
